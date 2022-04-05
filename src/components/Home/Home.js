c2eea60e3a8037c83efbc378df37c029ea4adfcf
saba rahman <sabarahman.official@gmail.com>
2022-04-05 16:58:03 +0600

    site details and readme added

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -19,7 +19,7 @@ const Home = () => {
                 <div className='site-details'>
                     <h1>Your Next Camera <br />
                         <span style={styles}> Your Best Camera</span></h1>
-                    <p style={textStyles}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur praesentium at a doloribus, ullam, nobis sint maiores totam excepturi, voluptatem unde numquam odio vitae asperiores veritatis vero? Vero, similique vel? Consequatur molestiae sapiente dicta molestias, delectus natus eum unde, suscipit aut corporis quod reiciendis! Excepturi error nesciunt porro iusto ut.</p>
+                    <p style={textStyles}>Meet the world's fastest autofocus. The a6000 is a superb hybrid camera offering the quality of an interchangeable lens camera and ultra-fast shutter speed.</p>
                 </div>
                 <div className='site-image'>
                     <img src="https://www.startech.com.bd/image/cache/catalog/dslr-camera/sony/%20alpha-a6000/a6000-500x500.jpg" alt="" />
