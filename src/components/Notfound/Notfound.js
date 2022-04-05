9f564999611c0554d1d4c268639f523b0d40f915
saba rahman <sabarahman.official@gmail.com>
2022-04-05 20:11:09 +0600

    project completed

--- src/components/Notfound/Notfound.js
+++ src/components/Notfound/Notfound.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import './Notfound.css'
 
 const Notfound = () => {
     return (
-        <div>
+        <div className='not-found'>
             <h2>The page you are looking for is not available</h2>
             <h4>404</h4>
         </div>
